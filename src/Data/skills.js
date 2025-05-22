cf0cb0a460a89fd2d8dea3da4cb9c2dc8dbeecd6
Praneet Dixit <praneetdixit1234@gmail.com>
2025-05-22 14:37:31 +0530

    Update logo and resources

--- src/Data/skills.js
+++ src/Data/skills.js
@@ -4,7 +4,7 @@ const skills = [
   { name: "NodeJS", percentage: 70, icon: "node-js", solid: false },
   { name: "React", percentage: 50, icon: "react", solid: false },
   { name: "Angular", percentage: 50, icon: "angular", solid: false },
-  { name: "ExpressJS", percentage: 50, icon: "js", solid: true },
+  { name: "ExpressJS", percentage: 50, icon: "js", solid: false },
   { name: "MongoDB", percentage: 45, icon: "envira", solid: false },
   { name: "Python", percentage: 45, icon: "python", solid: false },
   { name: "SolidWorks", percentage: 60, icon: "gear", solid: true },
